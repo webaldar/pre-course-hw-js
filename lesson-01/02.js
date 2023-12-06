1bcd00b8d1dea09f605aefd06e6035acd7713b54
webaldar <webaldar@gmail.com>
2023-12-06 18:17:27 +0300

    bag fixed

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -7,7 +7,7 @@
 //  перезагрузкой страницы у тебя будет новое число.​
 
 let number1 = Math.floor(Math.random() * 100);
-if(number1 & 2 == 0){
+if(number1 % 2 == 0){
     console.log(`«${number1} Это четное число»` );
 } else {
     console.log(`«${number1} Это нечетное число»` );
