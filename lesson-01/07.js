04e654ff3dd2dd690608462449b37ccbd3f10a85
webaldar <webaldar@gmail.com>
2023-12-06 20:57:22 +0300

    the seventh task of 1 week

---
+++ lesson-01/07.js
@@ -0,0 +1,12 @@
+// Дано:
+
+// ​let iAm = 'я'
+// let krasavchik = "красавчик!";
+// 📋
+//   Выведи в консоль «Я красавчик!».
+// ❗ Обрати внимание: «Я» с заглавной.
+
+let iAm = 'я'
+let krasavchik = "красавчик!";
+
+console.log(`${iAm.toUpperCase()} ${krasavchik}`);
