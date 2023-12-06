fe6190218f3f63297bf74796e05f6eb5c237eb81
webaldar <webaldar@gmail.com>
2023-12-06 19:27:17 +0300

    the third task of 1 week

---
+++ lesson-01/03.js
@@ -0,0 +1,18 @@
+// Имея в распоряжении только:
+// ​let number = 1;
+// и используя лишь
+// --number   number--  ++number   number++
+// Получи в консоли 1,3,3,1 
+// ❗Нужно вызвать консоль ЧЕТЫРЕ раза
+// Пример:
+// console.log(number**); 
+// console.log(**number); 
+// console.log(number**); 
+// console.log(**number);
+
+let number = 1;
+
+console.log(number++); 
+console.log(++number); 
+console.log(number--); 
+console.log(--number); 
