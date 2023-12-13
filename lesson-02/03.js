98a82b1f675f4eb50bd32e43ecc0d560e6fcf53c
webaldar <webaldar@gmail.com>
2023-12-13 19:25:22 +0300

    the third task of 2 week

---
+++ lesson-02/03.js
@@ -0,0 +1,10 @@
+// code
+const giveMeRightNumbers = (value1, value2, maximum) => {
+    if (value1 === value2 || value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+giveMeRightNumbers(100, 200, 90);
+
